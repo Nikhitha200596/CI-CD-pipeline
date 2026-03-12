95fc0f2712db20a2cce67ddd283c86d37e1e9323
Nikhitha chandan <nikithamogaveera9@gmail.com>
2026-03-12 12:05:27 +0530

    Fix Dockerfile

---
+++ App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>My CI/CD Pipeline Project</h1>
+      <p>This project demonstrates a Vite + React application deployed using Docker and Jenkins.</p>
+    </div>
+  )
+}
+
+export default App
